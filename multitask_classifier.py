81c65dbb42b440716d328e30787fac3c14eb6f12
Prabhjot Singh Rai <hi@psr.ai>
2024-05-21 15:38:28 -0500

    WIP: working on the multitask classifier

--- multitask_classifier.py
+++ multitask_classifier.py
@@ -73,7 +73,11 @@ class MultitaskBERT(nn.Module):
                 param.requires_grad = True
         # You will want to add layers here to perform the downstream tasks.
         ### TODO
-        raise NotImplementedError
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.sentiment_classifier = nn.Linear(BERT_HIDDEN_SIZE, N_SENTIMENT_CLASSES)
+        self.paraphrase_classifier = nn.Linear(BERT_HIDDEN_SIZE * 2, 1)
+        self.similarity_classifier = nn.Linear(BERT_HIDDEN_SIZE * 2, 1)
+        
 
 
     def forward(self, input_ids, attention_mask):
@@ -83,7 +87,10 @@ class MultitaskBERT(nn.Module):
         # When thinking of improvements, you can later try modifying this
         # (e.g., by adding other layers).
         ### TODO
-        raise NotImplementedError
+
+        x = self.bert(input_ids, attention_mask)['last_hidden_state']
+        x = self.dropout(x)
+        return x
 
 
     def predict_sentiment(self, input_ids, attention_mask):
@@ -93,8 +100,8 @@ class MultitaskBERT(nn.Module):
         Thus, your output should contain 5 logits for each sentence.
         '''
         ### TODO
-        raise NotImplementedError
-
+        hidden_states = self.forward(input_ids, attention_mask)
+        return self.sentiment_classifier(hidden_states)
 
     def predict_paraphrase(self,
                            input_ids_1, attention_mask_1,
@@ -104,8 +111,10 @@ class MultitaskBERT(nn.Module):
         during evaluation.
         '''
         ### TODO
-        raise NotImplementedError
-
+        hidden_states_1 = self.forward(input_ids_1, attention_mask_1)
+        hidden_states_2 = self.forward(input_ids_2, attention_mask_2)
+        hidden_states = torch.cat((hidden_states_1, hidden_states_2), dim=1)
+        return self.paraphrase_classifier(hidden_states)
 
     def predict_similarity(self,
                            input_ids_1, attention_mask_1,
@@ -114,10 +123,10 @@ class MultitaskBERT(nn.Module):
         Note that your output should be unnormalized (a logit).
         '''
         ### TODO
-        raise NotImplementedError
-
-
-
+        hidden_states_1 = self.forward(input_ids_1, attention_mask_1)
+        hidden_states_2 = self.forward(input_ids_2, attention_mask_2)
+        hidden_states = torch.cat((hidden_states_1, hidden_states_2), dim=1)
+        return self.similarity_classifier(hidden_states)
 
 def save_model(model, optimizer, args, config, filepath):
     save_info = {
